2288ddce0a941643be60c120968a326122521b54
Elite35pp <davide870520@gmail.com>
2021-08-06 00:23:31 +0800

    重構 將登入，登出抽至共用函式

--- WebFormMiniSample/homework.Auth/AuthManager.cs
+++ WebFormMiniSample/homework.Auth/AuthManager.cs
@@ -50,11 +50,49 @@ namespace homework.Auth
         }
 
 
-        /// <summary> 清除登入 </summary>
+        /// <summary> 登出 </summary>
         public static void Logout()
         {
             HttpContext.Current.Session["UserLoginInfo"] = null; //清除登入資訊，導致登入頁
         }
 
+
+        /// <summary> 嘗試登入 </summary>
+        /// <param name="account"></param>
+        /// <param name="pwd"></param>
+        /// <param name="errorMsg"></param>
+        public static bool TryLogin(string account, string pwd, out string errorMsg)
+        {
+            //確認是否是空的
+            if (string.IsNullOrWhiteSpace(account) || string.IsNullOrWhiteSpace(pwd))
+            {
+                errorMsg = "帳號密碼為必填";
+                return false;
+            }
+
+
+            //讀取db 並檢查
+            var dr = UserInfoManager.GetUserInfoByAccount(account);
+
+            //確認是是空的    
+            if (dr == null)
+            {
+                errorMsg = $"帳號: {account}不存在";
+                return false;
+            }
+
+            //確認帳號密碼
+            if (string.Compare(dr["Account"].ToString(), account, true) == 0 && string.Compare(dr["PWD"].ToString(), pwd, false) == 0)
+            {
+                HttpContext.Current.Session["UserLoginInfo"] = dr["Account"].ToString();
+                errorMsg = string.Empty;
+                return true;
+            }
+            else
+            {
+                errorMsg = "登入失敗請檢察帳號密碼";
+                return false;
+            }
+        }
     }
 }
